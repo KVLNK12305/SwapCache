40614e476847b866a048018b4adead9768e270c2
Kushal Kurapati <kls2edmentre@gmail.com>
2025-06-08 23:35:48 +0530

    changes and errors solved

--- structures.py
+++ structures.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, key, data):
+        self.key = key
         self.val = data
-        # for the linked list
         self.prev = None
         self.next = None
-        # for mapping purpose
+
 class DLL:
     def __init__(self):
         self.length = 0
@@ -13,48 +13,95 @@ class DLL:
 
     def size_dll(self):
         return self.length
-    
-    def insert_first(self,data):
-        new_node = Node(data)
+
+    def insert_first(self, node):
+        node.next = self.head
+        node.prev = None
         if self.head:
-            new_node.next = self.head
-            self.head.prev = new_node
-        self.head = new_node
-        self.length+=1
+            self.head.prev = node
+        self.head = node
+        if self.tail is None:
+            self.tail = node
+        self.length += 1
 
-    def insert_last(self,data):
-        new_node = Node(data)
+    def insert_last(self, node):
+        node.prev = self.tail
+        node.next = None
         if self.tail:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
+            self.tail.next = node
+            self.tail = node
         else:
-            self.head = new_node
-        self.length+=1
-
-        self.length+=1
+            self.head = self.tail = node
+        self.length += 1
 
     def delete_first(self):
         if self.length == 0:
             raise Exception("Cannot perform operation on an empty list")
-        self.head = self.head.next  
+        node = self.head
+        self.head = self.head.next
         if self.head:
-            self.head.prev = None 
+            self.head.prev = None
         else:
-            self.head = None 
+            self.tail = None
         self.length -= 1
+        return node
 
     def delete_last(self):
         if self.length == 0:
             raise Exception("Cannot perform operation on an empty list")
+        node = self.tail
         self.tail = self.tail.prev
         if self.tail:
             self.tail.next = None
         else:
             self.head = None
         self.length -= 1
+        return node
+
     def show_values(self):
         curr = self.head
         while curr:
-            print(curr.val)
+            print(f"{curr.key}:{curr.val}")
             curr = curr.next
+
+    def remove_node(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+        self.length -= 1
+
+    def move_to_front(self, node):
+        self.remove_node(node)
+        self.insert_first(node)
+
+class LRU:
+    def __init__(self, C):
+        self.capacity = C
+        self.map = {}
+        self.dll = DLL()
+
+    def get(self, key):
+        if key in self.map:
+            node = self.map[key]
+            self.dll.move_to_front(node)
+            return node.val
+        return -1
+
+    def put(self, key, value):
+        if key in self.map:
+            node = self.map[key]
+            node.val = value
+            self.dll.move_to_front(node)
+        else:
+            if len(self.map) >= self.capacity:
+                lru_node = self.dll.delete_last()
+                if lru_node:
+                    del self.map[lru_node.key]
+            new_node = Node(key, value)
+            self.dll.insert_first(new_node)
+            self.map[key] = new_node
